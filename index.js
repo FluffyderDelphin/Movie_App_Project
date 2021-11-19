5bfc7f8805150c406b2d72d4eff128f0ef1d5176
Alexander Urlich <ulrichalex12@yahoo.de>
2021-11-19 13:24:25 +0100

    implementet basic express function, using also morgan

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('This is just a Test.');
+});
+
+app.use(express.static('public'));
+
+app.get('/movies', (req, res) => {
+  res.json(top10movies);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+});
+app.listen(8080, () => {
+  console.log('App ist Running on Port 8080');
+});
