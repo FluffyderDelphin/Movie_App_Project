4b508900205b6ce02553708cee8b4402053bc773
Alexander Urlich <ulrichalex12@yahoo.de>
2021-12-11 22:42:52 +0100

    fixed typos and placements to fix login issues

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let auth = require('./auth')(app);
-
 const passport = require('passport');
 require('./passport');
+app.use(passport.initialize());
+require('./auth')(app);
+
 // Get Movie List
 app.get(
   '/movies',
