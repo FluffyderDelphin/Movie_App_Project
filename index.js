ac78ff3cf48465b966b463762d831ce83345588a
Alexander Urlich <ulrichalex12@yahoo.de>
2022-05-01 17:11:26 +0200

    fixed typo

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 /** 
 /*  @swagger 
-/*  /Movies:
+/*  /movies:
 /*  get:
 /*     description: Get Movie list
 /*     responses: 
